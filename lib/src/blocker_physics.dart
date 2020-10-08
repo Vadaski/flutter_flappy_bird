19b2d2fa97e3c521371f3d31ac0aa2502af2450e
alexv525 <github@alexv525.com>
2020-10-09 00:19:05 +0800

    :sparkles: Implement blocker physics.

--- lib/src/blocker_physics.dart
+++ lib/src/blocker_physics.dart
@@ -2,6 +2,10 @@
 /// [Author] Alex (https://github.com/AlexV525)
 /// [Date] 2020/10/8 22:48
 ///
-class BlockerPhysics {
+import 'package:flutter/material.dart';
 
+abstract class BlockerPhysics {
+  BlockerPhysics({required this.size});
+
+  final Size size;
 }
